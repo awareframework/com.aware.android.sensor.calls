80efe7de0586318fc0dcf64cddd7afd339c6e19d
Sercan Turkmen <sercanturkmen@outlook.com>
2018-08-16 15:37:48 +0300

    example code in the test

---
+++ library/src/androidTest/java/com/awareframework/android/sensor/calls/ExampleInstrumentedTest.kt
@@ -0,0 +1,45 @@
+package com.awareframework.android.sensor.calls
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import com.awareframework.android.core.db.Engine
+import com.awareframework.android.sensor.calls.model.CallData
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ * <p>
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        CallsSensor.startService(appContext, CallsSensor.CallsConfig().apply {
+            sensorObserver = object : CallsSensor.SensorObserver {
+                override fun onCall(data: CallData) {
+                    // your code here...
+                }
+
+                override fun onRinging(number: String?) {
+                    // your code here...
+                }
+
+                override fun onBusy(number: String?) {
+                    // your code here...
+                }
+
+                override fun onFree(number: String?) {
+                    // your code here...
+                }
+            }
+            dbType = Engine.DatabaseType.ROOM
+            debug = true
+            // more configuration...
+        })
+    }
+}
